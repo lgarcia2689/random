68ee90f4c7101ba8950570700f93c7ef0f26a2b7
Luis Garcia <jrluis21@gmail.com>
2022-01-12 16:56:44 -0600

    created val

--- random-app/src/Gallery.js
+++ random-app/src/Gallery.js
@@ -2,10 +2,38 @@ import './Gallery.css'
 
 function Gallery() {
     return (
+        
+        <div className='galleryPage'>
+            <style>
+@import url('https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap');
+</style>
+            <h1>As you can tell I made this whole website for one thing only</h1>  
+            <img src='https://thumbs.dreamstime.com/b/man-women-doing-social-distancing-eating-food-alone-tables-restaurant-new-normal-make-blank-space-to-prevent-183385250.jpg' alt ="people eating" width="100%" />
+            <h1>I think your awesome, and I was thinking that instead of spending Valentine's day alone this year</h1>
+            <img src='https://i.pinimg.com/originals/39/dc/aa/39dcaa651bcaad8855f721c3be86f4f8.jpg' alt='couple eating together' width="100%" />
+            <h1>I want you to be my Valentine</h1>
+            <img src='https://media.istockphoto.com/vectors/will-you-be-my-valentine-word-and-rose-vector-illustration-vector-id910023036?b=1&k=20&m=910023036&s=612x612&w=0&h=ANOX50ONKhtGk8DqcyHW34IbBdI2Og-NLr635wkYigo=' alt="be my valentine" width="100%" />
+            <h1>Reasons you should say yes</h1>
 
-        <div>
-            <h1>Different Gallery</h1>  
-            
+
+
+            <div className='reasons'>
+            <div className='list'>
+            <ul >
+                <li>Probably none lol</li>
+                <li>Im funny sometimes</li>
+                <li>I think your neat</li>
+                <li>It would be fun</li>
+                <li>Free food</li>
+                <li>SAY YESS!!!!!</li>
+            </ul>
+            </div>
+            <div>
+            <img src='https://lobsteranywhere.com/wp-content/uploads/2015/05/Lobster-Tails-Steak-Shrimp-Dinner-1200x720.jpg' alt= "steak and lobster" width="80%"/>
+            </div>
+            </div>
+            <h1>So if your answer is Yes then shoot me a message or something lol, or just give me your answer:)</h1>
+
         </div>
     );
   }
